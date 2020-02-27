bb573dec5b94657e0d42bc15fec5faf4083ba439
vinicius <cleytonterceiro@outlook.com>
2020-02-26 21:01:43 -0300

    usando const module. Lesson 33

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,123 +1,135 @@
-const memoryCard = () => {
-  const $head = document.querySelector("head");
-  const $style = document.createElement("style");
-  $style.textContent = `
+const memoryCard = (function() {
+  const module = {};
 
-  .memory-card  {
-      width: 155px;
-      height: 155px;
-      position: relative;
-    }
-
-    .card {
-      width: 100%;
-      height: 100%;
-      background-color: #f25a70;
-      border-radius: 30px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-      position: absolute;
-      cursor:pointer;
+  module.create = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+    $style.textContent = `
+  
+    .memory-card  {
+        width: 155px;
+        height: 155px;
+        position: relative;
+      }
+  
+      .card {
+        width: 100%;
+        height: 100%;
+        background-color: #f25a70;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+        position: absolute;
+        cursor:pointer;
+      
+      }
+  
+      .memory-card.-active .card, .memory-card.-score .card {
+        display: none;
+      }
+  
+      .memory-card.-active .card.-front, .memory-card.-score .card.-front {
+        display: flex;
+      }
     
-    }
+      .memory-card .card.-front {
+        background-color: #fff;
+     
+      }
+    
+      .memory-card .card.-front::before {
+        content: "";
+        width: 95px;
+        height: 95px;
+        background-color: #d4d4d4;
+        border-radius: 50%;
+        position: absolute;
+      }
+    
+      .memory-card .card > .icon {
+        width: 100px;
+        height: 100px;
+      }
+    
+      .memory-card .card.-front > .icon {
+        position: absolute;
+        transform: translateY(-10px);
+      }
+    
+    `;
 
-    .memory-card.-active .card, .memory-card.-score .card {
-      display: none;
-    }
+    $head.insertBefore($style, null);
 
-    .memory-card.-active .card.-front, .memory-card.-score .card.-front {
-      display: flex;
-    }
-  
-    .memory-card .card.-front {
-      background-color: #fff;
-   
-    }
-  
-    .memory-card .card.-front::before {
-      content: "";
-      width: 95px;
-      height: 95px;
-      background-color: #d4d4d4;
-      border-radius: 50%;
-      position: absolute;
-    }
-  
-    .memory-card .card > .icon {
-      width: 100px;
-      height: 100px;
-    }
-  
-    .memory-card .card.-front > .icon {
-      position: absolute;
-      transform: translateY(-10px);
-    }
-  
-  `;
+    return ({ src, alt, nameClass }) =>
+      ` 
+      <div class="memory-card " onclick="memoryCard.handleClick(this)">
+        <article  class="card -front">
+        <img 
+            src = ${src}
+            alt = ${alt}
+            class="icon"
+            />
+        </article>
+        <article  class="card">
+        <img 
+            src = '../../../img/icon-collabcode.png'
+            alt = 'icone collabcode'
+            class="icon"
+            />
+        </article>
+       </div>
+     `;
+  };
 
-  $head.insertBefore($style, null);
-
-  return ({ src, alt, nameClass }) =>
-    ` 
-    <div class="memory-card " onclick="handleClick(this)">
-      <article  class="card -front">
-      <img 
-          src = ${src}
-          alt = ${alt}
-          class="icon"
-          />
-      </article>
-      <article  class="card">
-      <img 
-          src = '../../../img/icon-collabcode.png'
-          alt = 'icone collabcode'
-          class="icon"
-          />
-      </article>
-     </div>
-   `;
-};
-
-const handleClick = $el => {
-  if (!$el.classList.contains("-active") && !$el.classList.contains("-score")) {
-    activeMemoryCard($el);
-    checkSure();
-  }
-};
-
-function activeMemoryCard($el) {
-  if (qtdActiveMemoryCard < 2) {
-    $el.classList.add("-active");
-  }
-}
-
-function checkSure() {
-  if (qtdActiveMemoryCard === 1) {
-    const $memoryCards = document.querySelectorAll(".memory-card.-active");
+  module.handleClick = $el => {
     if (
-      $memoryCards[0].querySelector(".-front .icon").getAttribute("src") ===
-      $memoryCards[1].querySelector(".-front .icon").getAttribute("src")
+      !$el.classList.contains("-active") &&
+      !$el.classList.contains("-score")
     ) {
-      store.score++;
-      $memoryCards.forEach($memoryCard => {
-        $memoryCard.classList.add("-score");
-        $memoryCard.classList.remove("-active");
-      });
+      module.activeMemoryCard($el);
+      module.checkSure();
+    }
+  };
 
-      console.log(store.score);
-    } else {
-      setTimeout(() => {
-        const $ActiveMemoryCards = document.querySelectorAll(
-          ".memory-card.-active"
-        );
+  module.activeMemoryCard = $el => {
+    if (qtdActiveMemoryCard < 2) {
+      $el.classList.add("-active");
+    }
+  };
 
-        $ActiveMemoryCards.forEach($memoryCard => {
+  module.checkSure = () => {
+    if (qtdActiveMemoryCard === 1) {
+      const $memoryCards = document.querySelectorAll(".memory-card.-active");
+      if (
+        $memoryCards[0].querySelector(".-front .icon").getAttribute("src") ===
+        $memoryCards[1].querySelector(".-front .icon").getAttribute("src")
+      ) {
+        store.score++;
+        $memoryCards.forEach($memoryCard => {
+          $memoryCard.classList.add("-score");
           $memoryCard.classList.remove("-active");
         });
-        qtdActiveMemoryCard = 0;
-      }, 1000);
+
+        console.log(store.score);
+      } else {
+        setTimeout(() => {
+          const $ActiveMemoryCards = document.querySelectorAll(
+            ".memory-card.-active"
+          );
+
+          $ActiveMemoryCards.forEach($memoryCard => {
+            $memoryCard.classList.remove("-active");
+          });
+          qtdActiveMemoryCard = 0;
+        }, 1000);
+      }
     }
-  }
-}
+  };
+
+  return {
+    create: module.create,
+    handleClick: module.handleClick
+  };
+})();
