d88cef252625a0cba86e06bee3cc59ad4ce01b27
vinicius <cleytonterceiro@outlook.com>
2020-03-01 18:09:43 -0300

    criado o componente de contagem, Lesson 34

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,8 +2,9 @@
   const $root = document.querySelector("#root");
 
   const $cardsWrapper = createCardsWrapper();
-
   const createMemoryCard = memoryCard.create();
+
+  const $pointBar = pointBar.create();
 
   const $memoryCardC = createMemoryCard({
     src: "img/icon-c.png",
@@ -31,5 +32,7 @@
   $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
   $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJs);
   $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+
+  $root.insertAdjacentHTML("afterbegin", $pointBar);
   $root.insertAdjacentElement("beforeend", $cardsWrapper);
 })();
